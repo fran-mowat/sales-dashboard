6bd26db5af49c17dfefee0a2dfbcb028640f4e18
fran0770 <franmowat33@gmail.com>
2024-01-14 10:59:01 +0000

    checking window width to adjust content on window resize

--- script.js
+++ script.js
@@ -15,5 +15,16 @@ let closeMenu = (e) => {
     document.body.removeEventListener("click", closeMenu);
 }
 
+let checkWindow = () => {
+    if (window.innerWidth < 992 || document.body.clientWidth < 992) {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "grid"; //to allow for resizing of window
+        document.body.removeEventListener("click", closeMenu);
+    }
+}
+
+window.onresize = function() {checkWindow()}
+
 let hamburger = document.getElementById("menu");
 hamburger.addEventListener("click", openMenu);
