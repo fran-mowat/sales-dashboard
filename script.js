1d16b4cb09674fdf5cc878d5dd665981fbf68964
fran0770 <franmowat33@gmail.com>
2024-01-14 11:13:20 +0000

    removal of hamburger icon on menu display

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let openMenu = (e) => {
+    let hamburger = document.getElementById("menu");
+    hamburger.style.display = "none";
+    
     let sidebar = document.getElementById("sidebar");
     sidebar.style.display = "grid";
 
@@ -9,6 +12,9 @@ let openMenu = (e) => {
 let closeMenu = (e) => {
     e.stopPropagation();
 
+    let hamburger = document.getElementById("menu");
+    hamburger.style.display = "inline";
+
     let sidebar = document.getElementById("sidebar");
     sidebar.style.display = "none";
 
@@ -18,6 +24,7 @@ let closeMenu = (e) => {
 let checkWindow = () => {
     if (window.innerWidth < 992 || document.body.clientWidth < 992) {
         sidebar.style.display = "none";
+        hamburger.style.display = "inline";
     } else {
         sidebar.style.display = "grid"; //to allow for resizing of window
         document.body.removeEventListener("click", closeMenu);
